b18a05a67f88aac6ecc0c4a3f1e99dcc2c9d9546
Nur Rony <pro.nmrony@gmail.com>
2018-02-20 22:36:05 +0600

    add filter capability and allbook query

--- apollo-simple-express-server/server.js
+++ apollo-simple-express-server/server.js
@@ -1,28 +1,40 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import filter from 'lodash.filter'
 import { makeExecutableSchema } from 'graphql-tools'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 
-// make some data
+// make fake data
 const bookList = [
   {
+    id: 1,
     title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
+    author: 'J.K. Rowling'
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
+    id: 3,
+    title: "Harry Potter and the Prisoner of Azkabaan",
+    author: 'J.K. Rowling'
   },
-];
+  {
+    id: 2,
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+]
 
 const typeDefinitions = `
- type Query { books: [Book] }
- type Book { title: String, author: String }
+ type Query {
+   books(id: Int, author: String): [Book],
+   allBooks: [Book]
+  }
+ type Book { id: Int, title: String, author: String }
 `
 
 const resolvers = {
   Query: {
-    books: () => bookList
+    books: (_, book) => filter(bookList, book),
+    allBooks: () => bookList
   }
 }
 
@@ -31,11 +43,11 @@ const schema = makeExecutableSchema({ typeDefs: typeDefinitions, resolvers })
 const graphqlServer = express()
 
 // The GraphQL endpoint
-graphqlServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema,  }));
+graphqlServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
 
 // GraphiQL, a visual editor for queries
-graphqlServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+graphqlServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 graphqlServer.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+  console.log('Go to http://localhost:3000/graphiql to run queries!')
+})
