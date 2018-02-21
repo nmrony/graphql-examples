4f9b598d38bc610875955eedac15ff4638444677
Nur Rony <pro.nmrony@gmail.com>
2018-02-21 16:04:17 +0600

    configure apollo client

--- apollo-react-hackernews/src/index.js
+++ apollo-react-hackernews/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import './index.css'
+import App from './components/App/App'
+
+// create the link
+const uri = 'http://localhost:4000'
+const httpLink = new HttpLink({ uri })
+
+// create Apollo client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+const HackerNewsApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
+ReactDOM.render(<HackerNewsApp />, document.getElementById('root'))
 registerServiceWorker()
