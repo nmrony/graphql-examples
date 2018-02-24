243133282fa698f2d6a03671c5e3e1953d8c4d8a
Nur Rony <pro.nmrony@gmail.com>
2018-02-24 16:15:49 +0600

    feat(client): add login and signup functionality

--- apollo-react-hackernews/client-web/src/components/App/App.js
+++ apollo-react-hackernews/client-web/src/components/App/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import './App.css'
-import logo from './logo.svg'
 import Header from './../Header/Header'
 import { LinkList, CreateLink } from './../Link'
+import { Login } from './../Login'
 
 export class App extends Component {
   render() {
@@ -13,6 +13,7 @@ export class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
+            <Route exact path="/login" component={Login} />
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
           </Switch>
