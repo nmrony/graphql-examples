adcc87fd85a1766a4e08c5e6c0f2461ce1732f19
Nur Rony <pro.nmrony@gmail.com>
2018-04-07 20:57:26 +0600

    configure AppSync client

---
+++ appsync-recipes/README.md
@@ -0,0 +1,2 @@
+Simple Recipe App with AWS AppSync
+===================================
---
+++ appsync-recipes/src/appsync.js
@@ -0,0 +1,8 @@
+﻿const appSyncConfig = {
+  graphqlEndpoint: process.env.APPSYNC_API_URL || null,
+  region: process.env.AWS_REGION,
+  authenticationType: process.env.APPSYNC_AUTH_TYPE || 'API_KEY',
+  apiKey: process.env.APPSYNC_API_KEY
+}
+
+export default appSyncConfig
