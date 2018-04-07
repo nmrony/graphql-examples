34a97688f1c80178fa167bafe4e45eaf796392cf
Nur Rony <pro.nmrony@gmail.com>
2018-04-07 21:00:50 +0600

    add example env file

--- appsync-recipes/src/appsync.js
+++ appsync-recipes/src/appsync.js
@@ -1,6 +1,6 @@
 ﻿const appSyncConfig = {
   graphqlEndpoint: process.env.APPSYNC_API_URL || null,
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION || 'us-east-2',
   authenticationType: process.env.APPSYNC_AUTH_TYPE || 'API_KEY',
   apiKey: process.env.APPSYNC_API_KEY
 }
